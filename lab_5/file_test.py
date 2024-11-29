6a7255f2be6885a30737a2730ef27b389a397f5e
Klon <www.vfdrey@gmail.com>
2024-11-29 18:05:38 +0400

    first try

--- lab_5/file_test.py
+++ lab_5/file_test.py
@@ -25,7 +25,12 @@ def test_dataset():
     shutil.rmtree(dataset_path)
 
 
-def test_get_absolute_paths(test_dataset):
+def test_get_absolute_paths(test_dataset: str):
+    """Get absolute paths to the dataset and checks their correctness for length and content
+
+    Args:
+        test_dataset (str): path to dataset
+    """
     class_num = 1
     result = get_absolute_paths(class_num, test_dataset)
 
@@ -39,7 +44,13 @@ def test_get_absolute_paths(test_dataset):
     assert all(path in expected_files for path in result_paths)
 
 
-def test_get_relative_paths(test_dataset):
+def test_get_relative_paths(test_dataset: str):
+    """Get relative paths to the dataset and checks their correctness for length and content
+
+    Args:
+        test_dataset (str): path to dataset
+    """
+
     class_num = 2
     result = [os.path.normpath(path) for path in get_relative_paths(class_num, test_dataset)]
     expected_files = [
@@ -52,7 +63,12 @@ def test_get_relative_paths(test_dataset):
 
 
 
-def test_make_csv(test_dataset):
+def test_make_csv(test_dataset: str):
+    """make csv and check correctness for path and content
+
+    Args:
+        test_dataset (str): path to test dataset
+    """
     csv_path = "lab_5/paths.csv"
 
     make_csv(test_dataset)
